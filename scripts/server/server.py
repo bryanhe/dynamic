2ffc6090a9cdf6b197103676ba0ef0e499187b12
Bryan He <bryanhe@stanford.edu>
2021-07-14 15:45:13 -0700

    update interface

--- scripts/server/server.py
+++ scripts/server/server.py
@@ -32,56 +32,52 @@ def _index():
 
 @app.route("/patient/<string:patient>", methods=["GET", "POST"])
 def patient(patient):
-    output = os.path.join(SAVE_DIR, "{}.pkl".format(patient))
+    output = os.path.join(SAVE_DIR, "{}.tsv".format(patient))
+    os.makedirs(SAVE_DIR, exist_ok=True)
     if flask.request.method == "GET":
         view = {}
         if os.path.isfile(output):
             try:
                 with open(output, "r") as f:
                     for line in f:
-                        video, v = line.split("\t")
+                        video, v = line.strip().split("\t")
                         view[video] = v
             except Exception as e:
                 print(e)
                 data = {}
 
-        # videos = os.listdir(DATA_DIR)
-        # videos = sorted(map(lambda v: os.path.splitext(v)[0], videos))
-        # index = videos.index(video)
-        # total = len(videos)
-        # prev = None
-        # if index != 0:
-        #     prev = videos[index - 1]
-        # next = None
-        # if index + 1 < len(videos):
-        #    next = videos[index + 1]
-        # capture = cv2.VideoCapture(os.path.join(DATA_DIR, video + ".webm"))
-        # fps = capture.get(cv2.CAP_PROP_FPS)
-        # height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        # width = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+        patients = sorted(os.listdir(os.path.join(DATA_DIR, "Videos")))
+        index = patients.index(patient)
+        total = len(patients)
+        prev = None
+        if index != 0:
+            prev = patients[index - 1]
+        next = None
+        if index + 1 < len(patients):
+           next = patients[index + 1]
+
         videos = []
         for file in sorted(os.listdir(os.path.join(DATA_DIR, "Videos", patient))):
             if file not in ["full", "color", "complete.txt"]:
                 videos.append(file)
+                # capture = cv2.VideoCapture(os.path.join(DATA_DIR, "Videos", patient, "full", file))
+                # fps = capture.get(cv2.CAP_PROP_FPS)
+                # height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+                # width = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+                # print(height, width, fps)
         for i in range(len(videos)):
             if videos[i] in view:
                 videos[i] = (videos[i], view[videos[i]])
             else:
-                videos[i] = (videos[i], "-- Select View --")
+                videos[i] = (videos[i], "--Select View--")
 
-        return flask.render_template("patient.html", patient=patient, videos=videos)
+        return flask.render_template("patient.html", patient=patient, videos=videos, index=index, total=total, prev=prev, next=next, views=["--Select View--", "A4C", "PSAX"], view_of_video=view)
     else:
         data = flask.request.data
-        data = data.strip().decode("utf-8").split("\n")
-        print(data)
-        data = [d.split(":") for d in data]
-        print(data)
-        data = {key: value for (key, value) in data}
-        print(data)
-        os.makedirs(os.path.dirname(output), exist_ok=True)
+        data = data.decode("utf-8")
         # TODO: save to temp loc and move
-        with open(output, "wb") as f:
-            pickle.dump(data, f)
+        with open(output, "w") as f:
+            f.write(data)
 
         return ""
 
